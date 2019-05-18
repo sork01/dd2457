95dd656c37435af9e5e5660b6226f625f145e5e7
Mikael Forsberg <miforsb@kth.se>
2019-05-18 20:07:13 +0200

    clean up Main

--- lab2/semant/Main.java
+++ lab2/semant/Main.java
@@ -5,38 +5,22 @@ import semant.amsyntax.*;
 import semant.whilesyntax.Stm;
 import java.util.LinkedList;
 
-public class Main {
-    public static void main(String[] args) throws Exception {
-        Boolean step = false;
-        int n = args.length;
+public class Main
+{
+    public static void main(String[] args) throws Exception
+    {
         Stm s = WhileParser.parse(args[0]);
-        if (n == 2 && args[1].equals("s"))
-        {
-            step = true;
-        }
-
+        
         s.accept(new ControlPointAssigner());
         System.out.println("while: ");
         s.accept(new PrettyPrinter());
-        System.out.println("");
-        System.out.println("");
-        System.out.println("AM: ");
-        System.out.println("");
-        LinkedList<Inst> amcode = new LinkedList<Inst>();
+        System.out.println("\n");
+        
         Code code = new Code();
-        code = (s.accept(new ASTtoAM()));
-        // System.out.println("this is code " + code);
-        for (int i = 0; i < code.size(); i++)
-        {
-            amcode.addLast(code.get(i));
-        }
-        VM vm = new VM();
+        code = s.accept(new ASTtoAM());
+        
         AIVM aivm = new AIVM();
-        // System.out.println("this is amcode " + amcode);
-        //System.out.println(vm.Execute(amcode, step));
-        //aivm.step_to_next_controlpoint(amcode);
-        aivm.Execute(amcode, step);
-
-
+        
+        aivm.Execute(new LinkedList<Inst>(code));
     }
 }
